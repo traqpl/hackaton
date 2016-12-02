b4224077f5e3440635f731fcf27c7898305adb15
Daga <cos@tam.com>
2016-12-02 21:20:15 +0100

    My commit message

---
+++ zlicz_znaki.py
@@ -0,0 +1,28 @@
+from random import randint
+def zassij_slowo():
+    lista = []
+    f = open('slownik.txt', 'r')
+    ile_linii = 0
+    lines = f.readlines()
+    for line in lines:
+        lista.append(line.strip())
+        ile_linii += 1
+    f.close()
+    return(lista[randint(0,ile_linii - 1)])
+
+def dashe():
+    a = zassij_slowo()
+    znaki_w_slowie = len(a)
+    return"_ " * znaki_w_slowie
+
+
+# print(dashe())
+
+
+
+
+
+
+
+
+
