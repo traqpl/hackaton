45dc3dccff35f0664485e5afefe6f4da6683a3b1
Mjure <mateusz.jurewicz@gmail.com>
2016-12-02 20:40:49 +0100

    Added get_letter function in helpers.py that grabs letter from player and checks if it's valid

--- helpers.py
+++ helpers.py
@@ -3,23 +3,47 @@ import string
 letters = string.ascii_letters
 used_letters = ''
 
-def ask_letter(question, chosen_letters):
+def get_letter(question, chosen_letters):
+    """
+    Get a new letter from player. Step by step:
+        ask question
+        check if input is one character
+        check if input is a letter
+        convert input to lowercase
+        check if input is in used letters
+        return new chosen letter
+    :param question:
+        This is the question to be asked, when asking player for a new letter.
+    :param chosen_letters:
+        This is a list of letters already chosen by the player.
+    :return:
+        Returns one new letter, chosen by the player.
+    """
     while True:
+        # ask question
         choice = input(question)
-        try:
-            choice = str(choice)
-        except Exception as e:
-            print("Not a letter: ", e)
-        else:
-            if choice in chosen_letters:
-                print("You already chose ", choice)
-            elif choice not in letters:
-                print("That wasn't a letter!\n")
-            else:
-                print("Thanks for the letter: ", choice)
-                chosen_letters += choice
-                ''.join(sorted(chosen_letters))
-                print("Your chosen letters are: ", chosen_letters)
-                return choice, chosen_letters
 
-ask_letter("Give me a letter:\n", used_letters)
+        # check if input is one character
+        if len(choice) != 1:
+            print(choice, "isn't just one letter!\n")
+            continue
+
+        # check if input is a letter
+        if choice not in letters:
+            print(choice, "isn't a letter!\n")
+            continue
+
+        # convert input to lowercase
+        choice = choice.lower()
+
+        # check if input is in used letters
+        if choice in chosen_letters:
+            print(choice, "has already been chosen!\n")
+            continue
+
+        # return new chosen letter
+        return choice
+
+# tests
+if __name__ == '__main__':
+    print(get_letter("Gimme a letter!\n", 'abcdefghi'))
